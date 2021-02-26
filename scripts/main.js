f829a1a709b753b724a7790b271d3189b8b02d07
ColtMay <colten_mayberry@pm.me>
2021-02-26 13:09:08 -0600

    added tips into a test div of html

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
 import { FishList } from "./fish/FishList.js";
-
+import { Tip } from "./tips/Tip.js";
+import { TipList } from "./tips/TipList.js";
 FishList()
+TipList()
---
+++ scripts/tips/TipDataProvider.js
@@ -0,0 +1,11 @@
+const tipCollection = [
+    "Choose The Correct Size Tank For Your Fish.",
+    "Do Not Overstock Your Tank.",
+    "Choose A Proper Location For Your Tank: Away From Windows And Air Conditioning",
+    "Properly Cycle Your Tank",
+    "Perform Regular Water Changes"
+];
+
+export const getTips = () => {
+    return tipCollection;
+}
